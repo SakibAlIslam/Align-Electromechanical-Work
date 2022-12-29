3d265b10bfb58e5bdc95cc322f99da3272354b8d
SakibAlIslam <sakib35-1680@diu.edu.bd>
2022-12-29 20:52:33 +0600

    header done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/s103X0b/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
