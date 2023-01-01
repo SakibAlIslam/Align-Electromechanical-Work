85d10c811c07ab6003ae5832a6d56f68d5773780
SakibAlIslam <sakib35-1680@diu.edu.bd>
2023-01-01 23:44:48 +0600

    home page done

---
+++ helpers/data/executiveData.js
@@ -0,0 +1,18 @@
+export const executives = [
+  {
+    name: "Mr. Kutub Kapadia",
+    position: "Managing Director",
+    speech:
+      "With over 18 years experience in UAE in the field of MEP, he has handled several senior positions in Contracting and Consulting. He is a thorough professional and an experienced technocrat. He carries great respect in the market for providing excellent service and cost effective solutions.",
+    imgUrl:
+      "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+  {
+    name: "Mr. Tapan Desai",
+    position: "General Manager",
+    speech:
+      "He has strong Management & Projects background with over 2 decades of total experience in diverse fields of HVAC, Electrical, Low Voltage Systems, Fire Protection System and Fire water tanks.",
+    imgUrl:
+      "https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+];
